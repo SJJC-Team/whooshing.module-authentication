9d2e7c0eed25ce987586ae85392641a542547abf
ChenLin-Wang <wangchenlin2001@icloud.com>
2025-05-29 23:56:17 +0800

    V2.0.2.4 更新依赖

--- service/Package.swift
+++ service/Package.swift
@@ -1,38 +1,54 @@
 // swift-tools-version:6.0
 import PackageDescription
 
+// 设置该 Whooshing 服务模块的子模块
+// 指定某个环境变量，则需要在 configure.swift 中实现相关的配置函数
+// 可设置 .https 和 .api 两个
+let WhooshingModules: [WhooshingModuleType] = [
+    .https
+]
+
+enum WhooshingModuleType: String {
+    case https = "HTTPS"
+    case api = "API"
+}
+
 let package = Package(
-    name: "service",
+    name: "whooshing.template-pgsql",
     platforms: [
         .macOS(.v10_15),
-        .iOS(.v13),
+        .iOS(.v14),
+        .watchOS(.v6),
         .tvOS(.v13),
-        .watchOS(.v6)
     ],
     dependencies: [
+        // 💧 Vapor -- Swift 服务器端第三方框架
+        .package(url: "https://github.com/SJJC-Team/whooshing-vapor.git", from: "1.0.6"),
+        // 🪩 Whooshing 基本工具
+        .package(url: "https://github.com/SJJC-Team/whooshing.toolbox-basic.git", from: "1.3.7"),
+        // ⭐️ Whooshing 服务模块系统
+        .package(url: "https://github.com/SJJC-Team/whooshing.toolbox-server.git", .upToNextMajor(from: "1.0.12")),
         // 🔵 Swift 高性能网络通讯模块
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
-        // 💧 Vapor -- Swift 服务器端第三方框架
-        .package(url: "https://github.com/SJJC-Team/whooshing-vapor.git", .upToNextMajor(from: "1.0.1")),
-        // ⭐️ Whooshing 系统基本框架
-        .package(url: "https://github.com/SJJC-Team/whooshing.toolbox-basic.git", .upToNextMajor(from: "1.2.3")),
-        .package(url: "https://github.com/SJJC-Team/whooshing.toolbox-pgsql.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/SJJC-Team/whooshing.toolbox-server.git", .upToNextMajor(from: "1.0.0"))
+        // 🗄 关系型和非关系型数据库的 ORM(对象关系映射)
+        .package(url: "https://github.com/SJJC-Team/whooshing-fluent.git", from: "1.0.0"),
+        // 🐘 对 PostgreSQL 的 Fluent 驱动器
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.8.0"),
     ],
     targets: [
         .executableTarget(
             name: "App",
             dependencies: [
-                .product(name: "Vapor", package: "whooshing-vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
-                .product(name: "WhooshingServer", package: "whooshing.toolbox-server"),
-                .product(name: "PgSQL", package: "whooshing.toolbox-pgsql"),
-                .product(name: "WhooshingServer", package: "whooshing.toolbox-server"),
-                .product(name: "DataConvertable", package: "whooshing.toolbox-basic"),
+                .product(name: "Vapor", package: "whooshing-vapor"),
+                .product(name: "Fluent", package: "whooshing-fluent"),
+                .product(name: "Cryptos", package: "whooshing.toolbox-basic"),
                 .product(name: "ErrorHandle", package: "whooshing.toolbox-basic"),
+                .product(name: "WhooshingServer", package: "whooshing.toolbox-server"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
             ],
-            swiftSettings: swiftSettings + ["HTTPS"].map { .define($0) }
+            swiftSettings: swiftSettings
         ),
         .testTarget(
             name: "AppTests",
@@ -46,7 +62,10 @@ let package = Package(
     swiftLanguageModes: [.v5]
 )
 
-var swiftSettings: [SwiftSetting] { [
-    .enableUpcomingFeature("DisableOutwardActorInference"),
-    .enableExperimentalFeature("StrictConcurrency"),
-] }
+var swiftSettings: [SwiftSetting] {
+    [
+        .enableUpcomingFeature("DisableOutwardActorInference"),
+        .enableExperimentalFeature("StrictConcurrency")
+    ] +
+    WhooshingModules.map { SwiftSetting.define($0.rawValue) }
+}
